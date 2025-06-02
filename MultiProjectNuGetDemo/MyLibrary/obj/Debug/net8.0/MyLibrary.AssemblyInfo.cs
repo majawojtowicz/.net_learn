1728861f0dcf9c909a0b374b1ef39d0a8bf2afd4
maja.wojtowicz <wojtowiczmagdalenamaja@gmail.com>
2025-06-02 16:30:13 +0200

    New

--- MultiProjectNuGetDemo/MyLibrary/obj/Debug/net8.0/MyLibrary.AssemblyInfo.cs
+++ MultiProjectNuGetDemo/MyLibrary/obj/Debug/net8.0/MyLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0+ec49faa85c125fb14b6ef84e56ce6203b999f332")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0+f4e24bb119d2696bd252e1f8ae759ef91953b282")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.1.0.0")]
--- MultiProjectNuGetDemo/MyServices/obj/Debug/net8.0/MyServices.AssemblyInfo.cs
+++ MultiProjectNuGetDemo/MyServices/obj/Debug/net8.0/MyServices.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec49faa85c125fb14b6ef84e56ce6203b999f332")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4e24bb119d2696bd252e1f8ae759ef91953b282")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
